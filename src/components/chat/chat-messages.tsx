d9fd329e56b033e7c65d2b9f14302793d5a1164f
Ultiimaz <32419311+Ultiimaz@users.noreply.github.com>
2025-06-04 09:20:06 +0200

    Updated chat-messages.tsx

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -79,7 +79,7 @@ function MessageBubble({ message }: MessageBubbleProps) {
           <UserIcon className="h-5 w-5" />
         ) : (
           <div className="h-5 w-5">
-            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+            <svg stroke="currentColor\" fill="none\" strokeWidth="2\" viewBox="0 0 24 24\" strokeLinecap="round\" strokeLinejoin="round\" height="100%\" width="100%\" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 16c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"></path>
             </svg>
           </div>
